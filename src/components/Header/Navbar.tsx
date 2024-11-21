c9797e20a027295a559ea136a60a248a0e1d24cc
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2024-11-21 10:53:08 -0300

    fix: resolve navbar hover error

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -12,7 +12,7 @@ export const Navbar = () => {
           openMenu
             ? "opacity-100 pointer-events-auto"
             : "opacity-0 pointer-events-none"
-        } flex flex-col md:opacity-100 md:flex-row gap-6 max-md:absolute max-md:bg-white max-md:z-20 max-md:top-0 max-md:left-0 max-md:min-h-screen max-md:pt-20 max-md:px-5 max-md:w-[65%] transition-all duration-300`}
+        } flex flex-col md:opacity-100 md:pointer-events-auto md:flex-row gap-6 max-md:absolute max-md:bg-white max-md:z-20 max-md:top-0 max-md:left-0 max-md:min-h-screen max-md:pt-20 max-md:px-5 max-md:w-[65%] transition-all duration-300`}
       >
         {arrNavbar.map((item, i) => (
           <li key={i} className="relative group">
